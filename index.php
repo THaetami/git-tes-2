4b48202a948aa1297bc234937fe59ba4d1414360
Thaetami <tatanghaetami.97@gmail.com>
2021-11-03 16:39:10 +0700

    menambahkan list di file index.php

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba GIT REMOTE 2</title>
 </head>
+
 <body>
     <h4>Hello World</h4>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, minima iste nesciunt dolor dolores dolorum quod autem optio ratione. Aperiam itaque vero expedita ab molestiae reprehenderit eaque maiores optio beatae.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
+
 </html>
